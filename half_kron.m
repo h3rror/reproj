ec822c7dd293bdee2bedc3622a04e60492842181
hker <hker@cwi.nl>
2025-01-07 20:05:29 +0100

    plot condition number and norm of inverse of feature of rank suff basis

---
+++ half_kron.m
@@ -0,0 +1,15 @@
+function kron_h = half_kron(vector)
+% kronecker product of a vector with itslef without ambiguous entries:
+% a_i a_j = a_j a_i
+%
+% in incremental order: high index entries appear as late as possible
+
+n = numel(vector);
+
+counter = 1;
+for i = 1:n
+    for j = 1:i
+        kron_h(counter) = vector(i)*vector(j);
+        counter = counter +1;
+    end
+end
