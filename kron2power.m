1e340ab25a5cfed40eb6518e72e7ef4f3620b23a
hker <hker@cwi.nl>
2025-01-16 10:29:38 +0100

    make kron2power and power2kron sparse

--- kron2power.m
+++ kron2power.m
@@ -2,8 +2,13 @@ function I = kron2power(N)
 % computes matrix I_N^{(2)} as described in Peherstorfer reprojection
 % article
 
-I = eye(N^2);
+% I = eye(N^2);
 [~,~,u] = reduced_coordinates(N);
-I = I(u,:);
+% I = I(u,:);
 
 % test: kk = 3; kron2power(kk)*power2kron(kk)
+
+
+% sparse
+N_2 = N*(N+1)/2;
+I = sparse(1:N_2,u,1);
--- power2kron.m
+++ power2kron.m
@@ -2,10 +2,13 @@ function J = power2kron(N)
 % computes matrix J_N^{(2)} as described in Peherstorfer reprojection
 % article
 
-N_2 = N*(N+1)/2;
-
-J = eye(N_2);
+% N_2 = N*(N+1)/2;
+% 
+% J = eye(N_2);
 s = reduced_coordinates(N);
-J = J(s,:);
+% J = J(s,:);
 
 % test: kk = 3; kron2power(kk)*power2kron(kk)
+
+% sparse
+J = sparse(1:N^2,s,1);
