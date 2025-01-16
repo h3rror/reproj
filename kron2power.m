3104226ff63f7d14d1b7e9e76e5a7b41bcc89ce3
hker <hker@cwi.nl>
2025-01-16 10:36:19 +0100

    extend kron2power and power2kron to arbitrary power i

--- kron2power.m
+++ kron2power.m
@@ -1,9 +1,13 @@
-function I = kron2power(N)
+function I = kron2power(N,i)
 % computes matrix I_N^{(2)} as described in Peherstorfer reprojection
 % article
 
+if(nargin < 2)
+    i = 2;
+end
+
 % I = eye(N^2);
-[~,~,u] = reduced_coordinates(N);
+[~,~,u] = reduced_coordinates(N,i);
 % I = I(u,:);
 
 % test: kk = 3; kron2power(kk)*power2kron(kk)
--- power2kron.m
+++ power2kron.m
@@ -1,11 +1,15 @@
-function J = power2kron(N)
+function J = power2kron(N,i)
 % computes matrix J_N^{(2)} as described in Peherstorfer reprojection
 % article
+
+if(nargin < 2)
+    i = 2;
+end
 
 % N_2 = N*(N+1)/2;
 % 
 % J = eye(N_2);
-s = reduced_coordinates(N);
+s = reduced_coordinates(N,i);
 % J = J(s,:);
 
 % test: kk = 3; kron2power(kk)*power2kron(kk)
