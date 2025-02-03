3059c1479dbd6a6cbcd965b8d4c04f95c4ea5942
Henrik Rosenberger <hker@cwi.nl>
2025-02-03 19:36:03 +0100

    implement intrusive ROM computation and single-step FOM solver dummy

--- go_cubic.m
+++ go_cubic.m
@@ -3,6 +3,7 @@ close all;
 
 rng(1); % for reproducibility
 
+% N = 128;
 N = 12;
 
 %% Allen-Cahn equation as in https://doi.org/10.1016/j.cma.2022.115836
@@ -22,9 +23,12 @@ x0 = zeros(N,1);
 u_val = @(t) 10*(sin(pi*t)+1); % U_val
 
 dt = 1e-5;
-t_end = .1;
+% t_end = .1;
+% t_end = 1*dt;
+t_end = 20*dt;
 nt = t_end/dt;
 
+%% generate ROM basis construction data
 X_b = zeros(N,nt);
 U_b = zeros(1,nt);
 
@@ -42,5 +46,21 @@ for i=2:nt
 
     X_b(:,i) = x;
     U_b(:,i) = u;
+end
+%% construct ROM basis via POD
+[V,~,~] = svd(X_b,'econ');
+n = 3;
+Vn = V(:,1:n);
+
+%% construct intrusive operators
+tA1 = Vn'*A1*Vn;
+Jn3 = power2kron(n,3);
+% In3 = kron2power(n,3);
+tA3 = Vn'*A3*IN3*kron(Vn,kron(Vn,Vn))*Jn3;
+tB = Vn'*B;
+
+%% FOM solver running for one time step
+function x_1 = single_step(x_0,u_0)
+    x_1 = x_0 + dt*f(x_0,u_0);
 end
 
