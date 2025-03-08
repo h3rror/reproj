a3667f54cc63b592892a7e28294af7f34709a177
hker <hker@cwi.nl>
2025-03-08 18:19:15 +0100

    outsource getOpInfMatrix

---
+++ getOpInfMatrix.m
@@ -0,0 +1,24 @@
+function [D,A_inds,B_inds] = getOpInfMatrix(tX,U,is)
+
+D = U;
+ni = numel(is);
+n = size(tX,1);
+A_inds = zeros(ni,2);
+p = size(U,1);
+B_inds = [1 p];
+
+ind_old = p;
+for k = 1:ni
+    i = is(k);
+    if isa(tX,"int32")
+        [~,rows] = find(kron2power(n,i));
+        tXi = vecwise_kron(tX,i);
+        D = [D; tXi(rows,:)];
+    else
+        D = [D; kron2power(n,i)*vecwise_kron(tX,i)];
+    end
+    ind = size(D,1);
+    A_inds(k,:) = [ind_old+1, ind];
+    ind_old = ind;
+end
+D = sparse(double(D));
--- opinf.m
+++ opinf.m
@@ -9,28 +9,8 @@ function [O,A_inds,B_inds] = opinf(dot_tX,tX,U,is)
     % B_inds: 1 x 2 array of indices such that B = O(:,B_inds);
 
     % D = [];
-    D = U;
-    ni = numel(is);
-    n = size(tX,1);
-    A_inds = zeros(ni,2);
-    p = size(U,1);
-    B_inds = [1 p];
 
-    ind_old = p;
-    for k = 1:ni
-        i = is(k);
-        if isa(tX,"int32")
-            [~,rows] = find(kron2power(n,i));
-            tXi = vecwise_kron(tX,i);
-            D = [D; tXi(rows,:)]; 
-        else
-            D = [D; kron2power(n,i)*vecwise_kron(tX,i)];
-        end
-        ind = size(D,1);
-        A_inds(k,:) = [ind_old+1, ind];
-        ind_old = ind;
-    end
-    D = sparse(double(D));
+    [D,A_inds,B_inds] = getOpInfMatrix(tX,U,is);
 
    O = (D'\dot_tX')';
 end
