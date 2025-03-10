f145ca78d7b1cc90db6e7cbb6d6f3fe73bf8a5fa
hker <hker@cwi.nl>
2025-03-10 22:32:08 +0100

    plot number of snapshots comparisons

--- plot_number_snapshots.m
+++ plot_number_snapshots.m
@@ -3,21 +3,40 @@ close all;
 
 rng(1); % for reproducibility
 
+figure
+hold on
+
 %% burgers
 
-is = [1 2];
-% is_c = 0:max(is);
+% title("Burgers")
+% is = [1 2];
+% % is_c = 0:max(is);
+% 
+% n_trial = 5*10^4;
+% ns_trial = 1:10;
 
-n_trial = 5*10^5;
+%% chafee-infantee
+% title("Chafee-Infante")
+% is = [1 3];
+% 
+% n_trial = 10^7;
+% ns_trial = 1:12;
+% ylim([1 10^8])
+
+%% reaction-diffusion
+title("Reaction-diffusion")
+
+is = [1 2 3];
+n_trial = 5*10^3;
 ns_trial = 1:10;
+
 
 ns = 1:max(ns_trial)+10;
 
 n_is_ = n_is(ns,is);
 n_snaps = sum(n_is_,2);
 
-figure
-hold on
+
 plot(ns,n_snaps,'x-','LineWidth', 2,'DisplayName',"rank-suff")
 plot(ns_trial,n_trial*ones(size(ns_trial)),'x-','LineWidth', 2,'DisplayName',"trial and error")
 
@@ -25,6 +44,6 @@ set(gca, 'YScale', 'log')
 ylabel("number of snapshots")
 xlabel("ROM dimension")
 
-title("training error")
+% title("training error")
 grid on
 legend("show")
